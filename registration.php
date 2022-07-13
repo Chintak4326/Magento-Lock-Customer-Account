d94665b0cdae3fc3df6f13b181cb0e443de8be3f
Chintak4326 <chintakd999@gmail.com>
2022-07-14 02:15:53 +0530

    lock-customer-account-extension

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ChintakExtensions_CustomerLogin',
+    __DIR__
+);
